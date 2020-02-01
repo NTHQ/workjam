f412ac3da8a3e5d48679c55bcb88ae6e5d3d88f5
nthq <nguyen.thq.lewis@gmail.com>
2020-02-01 00:17:57 -0500

    Implemented factory class

--- src/main/java/com/workjam/interview/factories/UserFactory.java
+++ src/main/java/com/workjam/interview/factories/UserFactory.java
@@ -1,6 +1,11 @@
 package com.workjam.interview.factories;
 
+import com.workjam.interview.objects.user.Admin;
+import com.workjam.interview.objects.user.Employee;
+import com.workjam.interview.objects.user.Manager;
 import com.workjam.interview.objects.user.User;
+
+import java.time.LocalDate;
 
 public class UserFactory {
 
@@ -11,9 +16,29 @@ public class UserFactory {
      *
      * @return the new User created.
      */
-    public static User getUser() {
-
-        return null;
+    public static User getUser(User.Permission permission, LocalDate birthday, boolean active) {
+        switch (permission) {
+            case ADMIN:
+                Admin admin = new Admin();
+                admin.setName(getRandomName());
+                admin.setBirthDay(birthday);
+                admin.setActive(active);
+                return admin;
+            case MANAGER:
+                Manager manager = new Manager();
+                manager.setName(getRandomName());
+                manager.setBirthDay(birthday);
+                manager.setActive(active);
+                return manager;
+            case EMPLOYEE:
+                Employee employee = new Employee();
+                employee.setName(getRandomName());
+                employee.setBirthDay(birthday);
+                employee.setActive(active);
+                return employee;
+            default:
+                return null;
+        }
     }
 
     private static String getRandomName() {
